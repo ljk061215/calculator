2a76c8cc80801c378855e2c02049c138d1463076
JUNKAI LIU <389806507@qq.com>
2026-02-24 23:38:07 +0800

    Refactor calculator GUI with new layout and functions
    Enhanced calculator GUI with additional buttons and features.

--- calculator.py
+++ calculator.py
@@ -4,20 +4,43 @@ class GUI:
     def __init__(self):
         self.root=tk.Tk()
         self.root.title("Calculator")
-        self.root.geometry("210x40")
+        self.root.geometry("490x590")
         self.interface()
 
     #创建按钮、输入框、捆绑按键
     def interface(self):
-        self.entry=tk.Entry(self.root,width=20)
-        self.entry.grid(row=0,column=0,columnspan=9,ipady=4)
+        self.entry=tk.Entry(self.root,width=60,font=('Arial',11))
+        self.entry.grid(row=0,column=0,columnspan=4,ipady=10)
+        
+        button=[
+            ["绝对值","清空/Shift","撤回/back","/"],
+            ["7","8","9","*"],
+            ["4","5","6","-"],
+            ["1","2","3","+"],
+            ["00","0",".","=/enter"]
+            ]
+        
+        for i in range(len(button)):
+            for j in range(len(button[i])):
+                text=button[i][j]
+                btn=tk.Button(self.root,text=text,width=8,height=3,font=("Arial",16),command=lambda t=text:self.add_to_entry(t))
+                btn.grid(row=i+1,column=j,padx=5,pady=5)
 
-        self.entry.bind("<Return>",lambda event:self.colculate())
+        self.entry.bind("<Return>",lambda event:self.calculate())
+        self.entry.bind("<Shift_L>",lambda event:self.clean())
+        self.entry.bind("<Shift_R>",lambda event:self.abs())
 
-        self.Button0=tk.Button(self.root,text="清空/Shift",command=self.clean)
-        self.Button0.grid(row=0,column=10,ipady=1)
-
-        self.Button0.bind("<Shift_L><Shift_R>",lambda event:self.clean())
+    def add_to_entry(self,text):
+        if text=="=/enter":
+            self.calculate()
+        elif text=="清空/Shift":
+            self.clean()
+        elif text=="绝对值":
+            self.abs()
+        elif text=="撤回/back":
+            self.back()
+        else:
+            self.entry.insert(tk.END,text)
 
     #计算
     def calculate(self,event=None):
@@ -28,6 +51,17 @@ class GUI:
     #清空输入框
     def clean(self):
         self.entry.delete(0,tk.END)
+
+    #绝对值
+    def abs(self):
+        cal=self.entry.get()
+        self.entry.delete(0,tk.END)
+        self.entry.insert(0,abs(eval(cal)))
+
+    def back(self):
+        cal=self.entry.get()
+        self.entry.delete(0,tk.END)
+        self.entry.insert(0,cal[:-1])
 
 if __name__=="__main__":
     a=GUI()
